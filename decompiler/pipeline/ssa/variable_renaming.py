ca7280a58581a3acdda3eaad2816f9ba839563c9
J.Schröder <123640863+JannisIThink@users.noreply.github.com>
2026-03-27 04:18:38 +0100

    fix write

--- decompiler/pipeline/ssa/variable_renaming.py
+++ decompiler/pipeline/ssa/variable_renaming.py
@@ -704,7 +704,7 @@ class ConditionalVariableRenamer(VariableRenamer):
                         dependency_graph.remove_edges_from(edges)
                 #print("FailCount:",failCount)
                 path = os.environ["HEINZ_PETER"]
-                with open(path) as f:
+                with open(path,mode="w") as f:
                     f.write(f"{failCount};{len(ifPairs)};\n")
                 return dependency_graph
 
